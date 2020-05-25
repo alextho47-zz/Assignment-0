097db4b568c7b230a8e4b0857191324ee1ff7fcc
alextho47 <45271125+alextho47@users.noreply.github.com>
2020-05-25 14:48:38 -0400

    Update titleCaseEdit.js

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,17 @@
 function titleCaseEdit(title) {
-  // Insert code here;
+  
+  var str_array = title.split(' ');
+  
+  var new_str_array = [];
+    
+  for(var x = 0; x < str_array.length; x++){
+    
+      new_str_array.push(str_array[x].charAt(0).toUpperCase()+str_array[x].slice(1));
+    
+  }
+  
+  return new_str_array.join(' ');
+  
 }
 
 // Do not edit this line;
