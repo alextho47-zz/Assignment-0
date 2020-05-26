29d1b2c4cdf373f87ab2f798f38e47f9efb91ce0
alextho47 <45271125+alextho47@users.noreply.github.com>
2020-05-26 17:26:12 -0400

    Update pairSum.js

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,29 @@
 function pairSum(nums, target) {
-  // Insert code here;
+    
+    if(nums.length <= 1) {
+        
+        throw "Error";
+        
+    }
+  
+    var target_reached = false;
+    
+    for(var x = 0; x < nums.length; x++) { //Compare the sum of every number in array to target
+    
+      for(var z = 0; z < nums.length; z++) {
+        
+          if (nums[x] + nums[z] === target) { 
+           
+              target_reached = true;
+              
+          }
+          
+      } //End nested loop
+    
+  } //End main loop
+    
+    return target_reached;
+    
 }
 
 // Do not edit this line;
