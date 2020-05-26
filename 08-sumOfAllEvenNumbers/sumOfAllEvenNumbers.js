522b1cca513e6c37fc6a6f995c76ab790b809a08
alextho47 <45271125+alextho47@users.noreply.github.com>
2020-05-26 17:14:40 -0400

    Update sumOfAllEvenNumbers.js

--- 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
+++ 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
@@ -1,5 +1,19 @@
 function sumOfAllEvenNumbers(nums) {
-  // Insert code here;
+  
+    var count = 0;
+    
+    for(var x = 0; x < nums.length; x++) {
+    
+      if ((nums[x] % 2) == 0) {
+          
+          count++;
+          
+      }
+    
+  }
+    
+    return count;
+    
 }
 
 // Do not edit this line;
