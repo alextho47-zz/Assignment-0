84b7d07b26b064fc298d6034e5b8b19165a7feab
alextho47 <45271125+alextho47@users.noreply.github.com>
2020-05-26 17:21:20 -0400

    Update isPalindrome.js

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,5 +1,13 @@
 function isPalindrome(word) {
-  // Insert code here;
+    
+  var re = /[\W_]/g;
+    
+  var lowRegStr = word.toLowerCase().replace(re, '');
+    
+  var reverseStr = lowRegStr.split('').reverse().join(''); 
+    
+  return reverseStr === lowRegStr;
+    
 }
 
 // Do not edit this line;
