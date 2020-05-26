7ec1cf7dd9a32bf55b5f87507807928a1387a367
alextho47 <45271125+alextho47@users.noreply.github.com>
2020-05-26 17:23:58 -0400

    Update productOfAnyAmountOfNumbers.js

--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -1,5 +1,11 @@
 function productOfAnyAmountOfNumbers(...args) {
-  // Insert code here;
+    
+  return args.reduce((previous, current) => {
+      
+    return previous * current;
+      
+  });
+    
 }
 
 // Do not edit this line;
