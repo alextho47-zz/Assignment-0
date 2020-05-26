c5191a8ffb646bd9f60ddf6d064634f2f4fc0b88
alextho47 <45271125+alextho47@users.noreply.github.com>
2020-05-26 17:19:48 -0400

    Update fizzBuzz.js

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,5 +1,31 @@
 function fizzBuzz(start, end) {
-  // Insert code here;
+    
+    var nums = [];
+    
+    for(var x = start; x <= end; x++) {
+    
+      if (((x % 5) == 0) && !((x % 3) == 0)) { //Multiple of 5 only "Buzz"
+          
+          nums.push("Buzz");
+          
+      } else if (((x % 3) == 0) && !((x % 5) == 0)) { //Multiple of 3 only "Fizz"
+          
+          nums.push("Fizz");
+          
+      } else if (((x % 3) == 0) && ((x % 5) == 0)) { //Multiple of 3 AND 5 "FizzBuzz"
+          
+          nums.push("FizzBuzz");
+          
+      } else { //Neither, use original number
+          
+          nums.push(x);
+          
+      }
+    
+  }
+    
+    return nums;
+    
 }
 
 // Do not edit this line;
