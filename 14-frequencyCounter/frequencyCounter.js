f8116b760b3ca06fcd754647cdac635e3e12471b
alextho47 <45271125+alextho47@users.noreply.github.com>
2020-05-26 17:22:08 -0400

    Update frequencyCounter.js

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,25 @@
 function frequencyCounter(word) {
-  // Insert code here;
+    
+  var freq = {};
+    
+    for (var x = 0; x < word.length; x++) {
+        
+        var character = word.charAt(x);
+        
+        if (freq[character]) {
+            
+           freq[character]++;
+            
+        } else {
+            
+           freq[character] = 1;
+            
+        }
+        
+    }
+
+    return freq;
+    
 }
 
 // Do not edit this line;
